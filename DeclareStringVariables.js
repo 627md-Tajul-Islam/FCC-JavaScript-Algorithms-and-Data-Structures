9adf4219c378e4749344a8e83b01d34e66a1e2c7
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-08-22 23:53:49 +0600

    added

---
+++ DeclareStringVariables.js
@@ -0,0 +1,3 @@
+//"your name" is called a string literal. A string literal, or string, is a series of zero or more characters enclosed in single or double quotes.
+var myFirstName = "my first name";
+var myLastName = "my last name";
