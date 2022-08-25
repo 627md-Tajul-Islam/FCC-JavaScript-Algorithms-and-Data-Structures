97f5d5727e3d7e2940fadcc1d15765169811ccab
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-08-26 00:21:33 +0600

    ok

--- UnderstandingUninitializedVariables.js
+++ UnderstandingUninitializedVariables.js
@@ -5,3 +5,11 @@ console.log(a)
 var undefined;
 
 //If you do a mathematical operation on an undefined variable your result will be NaN which means "Not a Number". 
+var undefined;
+var undefined;
+console.log(undefined+undefined);
+
+var a = 5;
+var undefined;
+c = a + undefined;
+console.log(c)
