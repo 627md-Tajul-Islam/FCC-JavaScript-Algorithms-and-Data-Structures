9ae7a1cee4ae11f04a576a741f4c2c0d480875f9
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-08-26 01:45:11 +0600

    ok

--- Declare_A_ReadOnlyVariableWithTheConstKeyword.js
+++ Declare_A_ReadOnlyVariableWithTheConstKeyword.js
@@ -2,6 +2,7 @@
 //You should always name variables you don't want to reassign using the const keyword
 
 const FAV_PET = "Cats";
+FAV_PET = "Dogs";
 console.log(FAV_PET);
 
 // এখানে মেইন ব্যাপার টা হচ্ছে যে const keyword যদি ব্যাবহার করা হয় তাহলে সেই ভেরিয়েবেল এবং তার মান পারমানেন্ট হয়ে যায়। আমরা সেই ভেরিয়েবেল টা ২য় বার তো আর ব্যাবহার করতে পারব না এবং ভেরিয়েবেল এর মান টাও আমরা ২য় বার সেট করতে পারব না
