50220eadef6a925ae71c4df583c939f5735ac651
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-08-26 02:19:32 +0600

    ok

---
+++ AddTwoNumbersWithJavaScript.js
@@ -0,0 +1,28 @@
+//Number is a data type in JavaScript which represents numeric data.
+
+// using var
+var a = 5;
+var b = 5;
+var c = a + b;
+console.log(c);
+// at once
+var j = 5 + 5;
+console.log(j);
+
+// using let 
+let d = 5;
+let e = 5;
+let f = d + e;
+console.log(f);
+// at once
+let k = 5 + 5;
+console.log(k);
+
+// using const 
+const g = 5;
+const h = 5;
+var i = g + h;
+console.log(i);
+// at once
+const l = 5 + 5;
+console.log(l);
