57f14d0d65a978871b288cef5fd9b78674154c5f
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-08-26 00:48:21 +0600

    ok

--- ExploreDifferencesBetweenTheVarWithLetKeywords.js
+++ ExploreDifferencesBetweenTheVarWithLetKeywords.js
@@ -1,4 +1,10 @@
 //declaring variables with the var keyword is that you can easily overwrite variable declarations
-var camper = "James";
-var camper = "David";
+//var camper = "James";
+//var camper = "David";
+//console.log(camper);
+//the camper variable is originally declared as James, and is then overridden to be David. The console then displays the string David.
+
+let camper = "James";
+let campers = "David";
 console.log(camper);
+console.log(campers);
