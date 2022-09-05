ee0829baf2cd85be84544114f3ad42c045d1e579
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-05 15:40:36 +0600

    js added

---
+++ CompoundAssignmentWithAugmentedMultiplication.js
@@ -0,0 +1,13 @@
+//The *= operator multiplies a variable by a number.
+let myVar = 10;
+myVar = myVar * 5;
+
+// this is a new method
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
