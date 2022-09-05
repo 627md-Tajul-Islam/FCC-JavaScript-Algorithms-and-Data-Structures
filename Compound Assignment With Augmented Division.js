b622019fdf52e092795f1027e7b4eff2bb3963b5
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-05 15:44:36 +0600

    ok

---
+++ Compound Assignment With Augmented Division.js
@@ -0,0 +1,13 @@
+// old method
+let myVar = 10;
+myVar = myVar / 5;
+console.log(myVar);
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
