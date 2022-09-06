37702e533e054e241cb5ca02ce46348e8120ce9c
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-06 07:14:25 +0600

    ok

---
+++ QuotingStringswithSingleQuotes.js
@@ -0,0 +1,8 @@
+// both are valid 
+const doubleQuoteStr = "This is a string"; 
+const singleQuoteStr = 'This is also a string';
+
+const conversation = 'Finn exclaims to Jake, "Algebraic!"';
+console.log(conversation);
+
+const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
