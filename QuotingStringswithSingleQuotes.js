4bc21eaa35a52c246ba41617038b7a554d863b03
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-06 07:16:51 +0600

    ok

--- QuotingStringswithSingleQuotes.js
+++ QuotingStringswithSingleQuotes.js
@@ -5,4 +5,4 @@ const singleQuoteStr = 'This is also a string';
 const conversation = 'Finn exclaims to Jake, "Algebraic!"';
 console.log(conversation);
 
-const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
