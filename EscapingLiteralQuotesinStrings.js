7ebb92492387d4d111fbdafa795bd126550d7b3a
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-06 06:56:13 +0600

    ok

---
+++ EscapingLiteralQuotesinStrings.js
@@ -0,0 +1,10 @@
+// string
+let a = "Tajul";
+let b = "Islam";
+
+
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+//console.log(sampleStr);
+
+const test = "I am a \"double quoted\" string inside \"double quotes\". ";
+console.log(test);
