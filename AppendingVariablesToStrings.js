7a995ae71d704484736bafd5189e0af5398a900f
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-20 00:56:17 +0600

    ok

--- AppendingVariablesToStrings.js
+++ AppendingVariablesToStrings.js
@@ -3,3 +3,9 @@ const Adj = "Sawarim";
 let Str = "KAwlu Kawlu ";
 Str += Adj;
 console.log(Str);
+
+// Change code below this line
+const someAdjective = "Very Fun";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+console.log(myStr);
