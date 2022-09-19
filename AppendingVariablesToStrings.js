b609d04d277cc040ad75e34f0d19a4c6bd47f1a5
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-20 00:54:22 +0600

    ok

---
+++ AppendingVariablesToStrings.js
@@ -0,0 +1,5 @@
+// ust as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+const Adj = "Sawarim";
+let Str = "KAwlu Kawlu ";
+Str += Adj;
+console.log(Str);
