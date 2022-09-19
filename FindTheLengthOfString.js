6b30fa44b5eaf0c0b2febef983441ac287b05603
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-20 01:00:12 +0600

    ok

---
+++ FindTheLengthOfString.js
@@ -0,0 +1,12 @@
+//You can find the length of a String value by writing .length after the string variable or string literal.
+
+console.log("Tajul Islam".length);
+//Note that the space character between "Tajul" and "Islam" is also counted.
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName;
+console.log(lastNameLength.length)
