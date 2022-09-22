1acf9e49cfb5926d8a7d59adce5e05e5fa383b9d
627md-Tajul-Islam <tajul.azazil@gmail.com>
2022-09-22 12:28:41 +0600

    ok

--- commenting.js
+++ commenting.js
@@ -2,3 +2,5 @@
 /* 
 Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does. 
 */
+
+// this a test
