faf07841699d67cab35c9201da131d0fb626e65a
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-23 08:59:30 +0600

    ok

---
+++ UseBracketNotationToFindTheNthCharacterInAString.js
@@ -0,0 +1,13 @@
+// You can also use bracket notation to get the character at other positions within a string.
+// computers start counting at 0, so the first character is actually the zeroth character.
+
+const firstName = "Ada";
+const secondLetterOfFirstName = firstName[1];
+console.log(secondLetterOfFirstName);
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+console.log(thirdLetterOfLastName);
