b22add7941672f03961ceb8dae7068a49ac34d82
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-23 07:05:37 +0600

    ok

--- FindTheLengthOfString.js
+++ FindTheLengthOfString.js
@@ -9,4 +9,5 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 lastNameLength = lastName;
-console.log(lastNameLength.length)
+lastNameLength = lastName.length;
+console.log(lastNameLength);
