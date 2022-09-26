8cbedeb5f9ebb7fe570c51dac0f33abee2e92793
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-26 20:18:28 +0600

    ok

---
+++ AccessArrayDataWithIndexes.js
@@ -0,0 +1,10 @@
+// We can access the data inside arrays using indexes.
+// Like strings, arrays use zero-based indexing, so the first element in an array has an index of 0.
+
+
+const program = ["hello", "world", 627];
+console.log(program[2]);
+
+// ! accessing a selected element
+const name = program[0];
+console.log(name);
