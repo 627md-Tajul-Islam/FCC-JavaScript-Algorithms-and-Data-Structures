350a8d800aa7adc59cda8db991d0ea8663c30011
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-09-26 19:58:29 +0600

    ok

---
+++ StoreMultipleValuesInOneVariableUsingJavaScriptArrays.js
@@ -0,0 +1,7 @@
+// With JavaScript array variables, we can store several pieces of data in one place.
+
+const friends = ["rakib","imtiaz","sadik","nowsad","sajjad","asifa"];
+console.log(friends);
+
+// Only change code below this line
+const myArray = ["rakib",13];
