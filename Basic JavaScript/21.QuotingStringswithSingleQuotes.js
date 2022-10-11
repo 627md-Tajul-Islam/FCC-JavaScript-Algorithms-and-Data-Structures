6255a2d7746f5e3da5d9e680e9924fb007d09700
627md-Tajul-Islam <tajul.azazil@gmail.com>
2022-10-12 01:34:27 +0600

    added

--- Basic JavaScript/21.QuotingStringswithSingleQuotes.js
+++ Basic JavaScript/21.QuotingStringswithSingleQuotes.js
@@ -6,3 +6,4 @@ const conversation = 'Finn exclaims to Jake, "Algebraic!"';
 console.log(conversation);
 
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr);
