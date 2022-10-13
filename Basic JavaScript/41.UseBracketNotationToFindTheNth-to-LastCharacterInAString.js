61cbf61ec7755daff72e0bb4964ca9d7e53affbe
627md-Tajul-Islam <tajul.azazil@gmail.com>
2022-10-13 19:51:26 +0600

    add

--- Basic JavaScript/41.UseBracketNotationToFindTheNth-to-LastCharacterInAString.js
+++ Basic JavaScript/41.UseBracketNotationToFindTheNth-to-LastCharacterInAString.js
@@ -1,5 +1,5 @@
 // You can use the same principle we just used to retrieve the last character in a string to retrieve the Nth-to-last character.
-const firstName = "Augusta";
+const firstName = "tajul";
 const thirdToLastLetter = firstName[firstName.length - 3];
 console.log(thirdToLastLetter);
 
@@ -8,4 +8,4 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
-console.log(secondToLastLetterOfLastName);
+//console.log(secondToLastLetterOfLastName);
