293116eab31e6d5539db46223ab2e41f0c847048
627md-Tajul-Islam <tajul.azazil@gmail.com>
2022-10-15 01:17:42 +0600

    added

---
+++ Basic JavaScript/48.ShoppingList.js
@@ -0,0 +1,12 @@
+const myList = [
+    ["Chocolate Bar", 5],
+    ["Candy", 10],
+    ["Toffy", 15],
+    ["chips", 20],
+    ["glass", 25],
+];
+
+const List = [
+    ["Chocolate Bar","Candy","Toffy","chips","glass"],
+    [5,20,15,20,25],
+];
