bfd870cc62861bd17accbd0207fc73cc0ef73949
627md-Tajul-Islam <tajul.azazil@gmail.com>
2022-10-15 02:10:17 +0600

    added

---
+++ Basic-JavaScript/51.ReturnAValueFromAFunctionWithReturn.js
@@ -0,0 +1,10 @@
+// We can pass values into a function with arguments. 
+// You can use a return statement to send a value back out of a function.
+
+function plusThree(number){
+    return number + 3;
+    //console.log( number + 5);
+}
+const answer = plusThree(7);
+console.log(answer);
+//plusThree(5);
