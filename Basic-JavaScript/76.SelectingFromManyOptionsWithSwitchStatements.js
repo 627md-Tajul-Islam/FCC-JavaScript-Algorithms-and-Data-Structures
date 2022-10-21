5a2a46b7f10e93b903ea416954c70e1d8ccb18fc
627md-Tajul-Islam <tajul.azazil@gmail.com>
2022-10-21 22:18:17 +0600

    add

--- Basic-JavaScript/76.SelectingFromManyOptionsWithSwitchStatements.js
+++ Basic-JavaScript/76.SelectingFromManyOptionsWithSwitchStatements.js
@@ -40,3 +40,25 @@ function caseInSwitch(val) {
     return answer;
   }
   caseInSwitch(1);
+
+
+  /* function  caseInSwitch(val) {
+    switch(val){
+
+        case 1:
+            return "alpha";
+            break;
+
+        case 2:
+            return "beta";
+            break;
+
+        case 3:
+            return "gamma";
+            break;
+
+        case 4:
+            return "delta";
+            break;
+    }
+    } */
