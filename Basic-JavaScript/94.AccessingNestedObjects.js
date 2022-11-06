1d8f03693ee34f63e24179c129cffae5e12e7298
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-11-07 00:22:18 +0600

    ok

---
+++ Basic-JavaScript/94.AccessingNestedObjects.js
@@ -0,0 +1,31 @@
+// The sub-properties of objects can be accessed by chaining together the dot or bracket notation.
+
+const ourStorage = {
+    "desk": {
+      "drawer": "stapler"
+    },
+    "cabinet": {
+      "top drawer": { 
+        "folder1": "a file",
+        "folder2": "secrets"
+      },
+      "bottom drawer": "soda"
+    }
+  };
+const draw1 = ourStorage.cabinet["top drawer"].folder2;
+const draw2 = ourStorage.desk.drawer;
+
+
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
