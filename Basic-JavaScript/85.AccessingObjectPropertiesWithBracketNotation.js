6f6aab234d09e8afe23e348168cfbf53ae77d1d9
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-11-16 22:08:51 +0600

    ok

--- Basic-JavaScript/85.AccessingObjectPropertiesWithBracketNotation.js
+++ Basic-JavaScript/85.AccessingObjectPropertiesWithBracketNotation.js
@@ -1,36 +1,10 @@
 // The second way to access the properties of an object is bracket notation ([])
-// If the property of the object you are trying to access has a space in its name, you will need to use bracket notation.
 // However, you can still use bracket notation on object properties without spaces.
 
-/* 
-Here is a sample of using bracket notation to read an object's property:
-
-const myObj = {
-  "Space Name": "Kirk",
-  "More Space": "Spock",
-  "NoSpace": "USS Enterprise"
-};
-
-myObj["Space Name"];
-myObj['More Space'];
-myObj["NoSpace"];
-*/
-
-// myObj["Space Name"] would be the string Kirk, myObj['More Space'] would be the string Spock, and myObj["NoSpace"] would be the string USS Enterprise.
-
-// Note that property names with spaces in them must be in quotes (single or double).
-
-const testObj = {
-    "an entree": "hamburger",
-    "my side": "veggies",
-    "the drink": "water"
-  };
-  
-  const entreeValue = testObj["an entree"];
-  const mySideValue = testObj["my side"];
-  const drinkValue = testObj["the drink"];
-  
-  
-  console.log(entreeValue);
-  console.log(mySideValue);
-  console.log(drinkValue);
+const myObject = {
+    "Space Name" : "Space invaders",
+     Space : "invaders"
+}
+const ok = myObject["Space Name"];
+const not = myObject["Space"]
+console.log(not);
