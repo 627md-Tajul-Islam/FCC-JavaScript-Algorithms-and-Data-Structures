3ca67b44f93ff5ce2eac04034aad3604ab22c94c
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-11-19 00:43:14 +0600

    pk

--- Basic-JavaScript/92.TestingObjectsForProperties.js
+++ Basic-JavaScript/92.TestingObjectsForProperties.js
@@ -1,29 +1,10 @@
-// Sometimes it is useful to check if the property of a given object exists or not.
-// We can use the .hasOwnProperty(prop name) method of objects to determine if that object has the given property name.
+// Sometimes it is useful to check if the property of a given object exists or not. 
+//We can use the .hasOwnProperty(prop name) method of objects to determine if that object has the given property name. 
 // .hasOwnProperty() returns true or false if the property is found or not.
 
-const myObj = {
-    top: "hat",
-    bottom: "pants"
+const tajul = {
+ Name : "tajul"
 }
 
-const ok = myObj.hasOwnProperty("top");
-console.log(ok);
-
-const ok2 = myObj.hasOwnProperty("middle");
-console.log(ok2);
-
-// The first hasOwnProperty returns true, while the second returns false.
-
-function checkObj(obj, checkProp) {
-
-    if(obj.hasOwnProperty(checkProp)) 
-    {
-      return obj[checkProp];
-    }
-    else
-    {
-      return "Not Found"
-    }
-    
-  }
+const check = tajul.hasOwnProperty("Name");
+console.log(check)
