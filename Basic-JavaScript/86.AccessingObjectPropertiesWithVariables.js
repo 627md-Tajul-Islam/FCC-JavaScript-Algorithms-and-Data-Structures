4482d266c8078248ecc99ce15e69d846d9f77e7f
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-11-18 22:42:09 +0600

    ok

--- Basic-JavaScript/86.AccessingObjectPropertiesWithVariables.js
+++ Basic-JavaScript/86.AccessingObjectPropertiesWithVariables.js
@@ -1,11 +1,11 @@
 // Another use of bracket notation on objects is to access a property which is stored as the value of a variable.
 
-const friends = {
+const Tajul = {
     Name : "Tajul",
-    Roll : 432568,
-    School : "JZS"
+    ROll : 432568,
+    Dept : "CMT"
 }
 
-const college = "Roll";
-const output = friends[college];
-console.log(output);
+const value = "Dept";
+const other = Tajul[value];
+console.log(other);
