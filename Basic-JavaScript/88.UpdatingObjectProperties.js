91c3ec5d5e600c3e7b5a993b98a563dbb0a54bcd
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-11-18 23:01:52 +0600

    ok

--- Basic-JavaScript/88.UpdatingObjectProperties.js
+++ Basic-JavaScript/88.UpdatingObjectProperties.js
@@ -1,15 +1,11 @@
 // After you've created a JavaScript object, you can update its properties at any time just like you would update any other variable. 
 //You can use either dot or bracket notation to update.
 
-const myDog = {
-    "name": "Coder",
-    "legs": 4,
-    "tails": 1,
-    "friends": ["freeCodeCamp Campers"]
-  };
-  
-  const name = myDog.name = "Happy Coder";
-  
-  const names = myDog["name"] = "Happy Coder";
-  
-  console.log(names);
+const doggo = {
+    Name : "Dogesh",
+    State : "KAnnada"
+}
+
+const updated = doggo.State = "Doge";
+console.log(updated);
+console.log(doggo);
