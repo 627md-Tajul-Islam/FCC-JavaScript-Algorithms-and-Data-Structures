dccf5642f0e7809e0a3ff1af4cb26ce8fc564575
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-11-18 23:08:06 +0600

    ok

--- Basic-JavaScript/89.AddNewPropertiesToAJavaScriptObject.js
+++ Basic-JavaScript/89.AddNewPropertiesToAJavaScriptObject.js
@@ -1,23 +1,9 @@
 // You can add new properties to existing JavaScript objects the same way you would modify them.
-const ourDog = {
-    "name": "Camper",
-    "legs": 4,
-    "tails": 1,
-    "friends": ["everything!"]
-  };
-  
- const newName = ourDog.bark = "bow-wow";
 
- const myDog = {
-    "name": "Happy Coder",
-    "legs": 4,
-    "tails": 1,
-    "friends": ["freeCodeCamp Campers"]
-  };
-  //console.log(myDog)
-  
-  const sound = myDog.bark = "woof";
-  const race = myDog["race"] = "nigga";
-
-  //console.log(sound)
-  console.log(myDog)
+const tajul = {
+    Name : "Tajlu",
+    Roll : 432568
+}
+const adding = tajul.department = "CMT";
+console.log(adding);
+console.log(tajul);
