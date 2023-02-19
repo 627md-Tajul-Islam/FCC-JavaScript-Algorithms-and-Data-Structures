dabb276b170f4754262f54f9c9cca376133b744c
Md. Tajul Islam <tajul.azazil@gmail.com>
2023-02-19 19:31:19 +0600

    ok

--- Basic-JavaScript/97.IterateWithJavaScriptWhileLoops.js
+++
@@ -1,2 +0,0 @@
-// You can run the same code multiple times by using a loop.
-// The first type of loop we will learn is called a while loop because it runs while a specified condition is true and stops once that condition is no longer true.
