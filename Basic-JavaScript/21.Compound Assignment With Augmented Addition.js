c9aca765292a35ca706c0250eb97bc193dce5010
627md-Tajul-Islam <tajul.azazil@gmail.com>
2023-02-21 18:48:44 +0600

    o

--- Basic-JavaScript/21.Compound Assignment With Augmented Addition.js
+++ Basic-JavaScript/21.Compound Assignment With Augmented Addition.js
@@ -2,6 +2,7 @@
 In programming, it is common to use assignments to modify the contents of a variable. Remember that everything to the right of the equals sign is evaluated first, so we can say:
 
 myVar = myVar + 5;
+
 to add 5 to myVar. Since this is such a common pattern, there are operators which do both a mathematical operation and assignment in one step.
 
 One such operator is the += operator.
@@ -11,3 +12,10 @@ myVar += 5;
 console.log(myVar);
 */
 
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
