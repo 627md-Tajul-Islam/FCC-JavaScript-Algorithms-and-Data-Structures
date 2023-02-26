b6ffb60efe57c74843241191fe482c8e9a46a8d1
Md. Tajul Islam <tajul.azazil@gmail.com>
2023-02-26 15:20:35 +0600

    ok

---
+++ Basic-JavaScript/31.Appending Variables to Strings.js
@@ -0,0 +1,10 @@
+// Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+/*
+const anAdjective = "awesome!";
+let ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+*/
+
+const someAdjective = "Awesome";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
